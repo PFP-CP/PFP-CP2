8f35c30c75876d983cc4ca32ec21366799dec06f
Abdllahtt <abdellahtt2@gmail.com>
2026-02-20 00:03:08 +0100

    finish animations for auth page

--- frontEnd/pfp_front/components/abstract_components/auth_form.tsx
+++ frontEnd/pfp_front/components/abstract_components/auth_form.tsx
@@ -10,6 +10,8 @@ import Image from 'next/image'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import RadioButton from "../ui/radio_input";
+import { error } from "console"
+import { isValid } from "react-datepicker/dist/dist/date_utils.js"
 type keyValues = "email" | "full_name" | "password";
 
 export const errMap= new Map([
@@ -30,38 +32,39 @@ export type FormType = {
   birth_date: string;
 }
 
-const handle_auth_submit =(e:React.SubmitEvent<HTMLFormElement>,type:string, form:FormType)=>{
-      e.preventDefault()
-      switch (type) {
-        case "login":
-          fetch('https://webhook.site/6bbaee91-6155-4cee-ad3c-ae865ee85dd3',{
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({email:form.email,password:form.password}),
-          mode:'no-cors'
-      })
-          break;
-        case "forget":
-          fetch('https://webhook.site/6bbaee91-6155-4cee-ad3c-ae865ee85dd3',{
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({email:form.email}),
-          mode:'no-cors'
-      })
-        break;
-        
-        case "signup":
-          fetch('https://webhook.site/76f931ac-e4f6-4db8-8a14-3546402f9a8d',{
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(form),
-        mode:'no-cors'
-         })
-        break
+const handle_auth_submit =(type:string, form:FormType)=>{
       
-        default:
+        switch (type) {
+          case "login":
+            fetch('https://webhook.site/837ffb73-9b65-418d-8664-51417e8f843d',{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({email:form.email,password:form.password}),
+            mode:'no-cors'
+        })
+            break;
+          case "forget":
+            fetch('https://webhook.site/837ffb73-9b65-418d-8664-51417e8f843d',{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({email:form.email}),
+            mode:'no-cors'
+        })
           break;
-      }
+
+          case "signup":
+            fetch('https://webhook.site/837ffb73-9b65-418d-8664-51417e8f843d',{
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(form),
+          mode:'no-cors'
+           })
+          break
+         
+          default:
+            break;
+        }
+      
     }
 
 
@@ -81,33 +84,62 @@ export default function AuthForm(){
   const [errors, setErrors] = useState({full_name:"",email:"", password:""})
   const [handledate, setDate] = useState<Date|null>(null);
   
-  const ref = useRef<HTMLDivElement>(null);
-const prevRect = useRef<DOMRect | null>(null);
+  const button_ref = useRef<HTMLDivElement>(null);
+  const inpt_ref = useRef<HTMLDivElement>(null);
+  const logo_ref = useRef<HTMLDivElement>(null);
+
+  const prevRect_btn = useRef<DOMRect | null>(null);
+  const prevRect_inpt = useRef<DOMRect | null>(null);
+  const prevRect_logo = useRef<DOMRect | null>(null);
 
    const getRect= ()=>{
-     prevRect.current=ref.current?.getBoundingClientRect() || null;
+    prevRect_btn.current=button_ref.current?.getBoundingClientRect() || null;
+    prevRect_inpt.current=inpt_ref.current?.getBoundingClientRect() || null;
+    prevRect_logo.current=logo_ref.current?.getBoundingClientRect() || null;
+
    }
 
-  // useLayoutEffect(()=>{
-  //   let new_pos = ref.current.getBoundingClientRect();
-  //   if(!new_pos) return;
+  useLayoutEffect(()=>{
+    let new_pos_button = button_ref.current?.getBoundingClientRect();
+    let new_pos_inpt = inpt_ref.current?.getBoundingClientRect();
+    let new_pos_logo = logo_ref.current?.getBoundingClientRect();
+
+    if(!new_pos_button) return;
+    if(!new_pos_inpt) return;
+    if(!new_pos_logo) return;
     
-  //   let dy = prevRect.current?.top- new_pos.top ;
+    let dy = prevRect_btn.current?.top- new_pos_button.top ;
+    let dy1 = prevRect_inpt.current?.top- new_pos_inpt.top ;
+    let dy2 = prevRect_logo.current?.top - new_pos_logo.top;
 
-  //   ref.current.style.transition = `transform 0s`
-  //   ref.current.style.transform = `translate(0,${dy}px)`;
+    button_ref.current.style.transition = `transform 0s`
+    button_ref.current.style.transform = `translate(0,${dy}px)`;
 
-  //   ref.current.getBoundingClientRect();
+    inpt_ref.current.style.transition = `transform 0s`
+    inpt_ref.current.style.transform = `translate(0,${dy1}px)`;
+
+    logo_ref.current.style.transition = `transform 0s`
+    logo_ref.current.style.transform = `translate(0,${dy2}px)`;
+
+    button_ref.current.getBoundingClientRect();
 
 
-  //   requestAnimationFrame(()=>{
-  //     ref.current.style.transition = `transform 300ms`;
-  //     ref.current.style.transform = `translate(0,0)`;
-  //   })
+    requestAnimationFrame(()=>{
+      button_ref.current.style.transition = `transform 600ms`;
+      button_ref.current.style.transform = `translate(0,0)`;
 
-  //   prevRect.current = new_pos;
+      inpt_ref.current.style.transition = `transform 600ms`;
+      inpt_ref.current.style.transform = `translate(0,0)`;
 
-  // })
+      logo_ref.current.style.transition = `transform 600ms`
+      logo_ref.current.style.transform = `translate(0,0)`;
+    })
+
+    prevRect_btn.current = new_pos_button;
+    prevRect_inpt.current = new_pos_inpt;
+
+
+  },[isLogin])
 
   const errorList:string[]=[];
   Object.keys(errors).map((key)=>{
@@ -116,60 +148,76 @@ const prevRect = useRef<DOMRect | null>(null);
 
   const resetFields=()=>{
     setForm({full_name:"",email:"",password:"",gender:"",location:"",birth_date:""});
-    setIsValid(true)
+    setDate(null);
+    setErrors({full_name:"",email:"",password:""});
+    setIsValid(true);
   }
   
 
    const handleSubmition= (e:React.SubmitEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    if(!isValid) return;
     if(isLogin && !isForget){
-      handle_auth_submit(e,"login",form)
+      handle_auth_submit("login",form)
     }else if(isLogin && isForget){
-      handle_auth_submit(e,"forget",form)
+      handle_auth_submit("forget",form)
     }else if(!isLogin){
-      handle_auth_submit(e,"signup",form)
+      handle_auth_submit("signup",form)
 
     }
+    
    } 
 
-  return(
+   const handleSubmitButton=()=>{
+    if(isLogin && !isForget){
+      return "Sign in";
+    }else if(isLogin && isForget){
+      return "Request reset link";
+    }else if(!isLogin){
+      return "Sign up";
 
-    
-    <div key={isLogin? 1:2} className={styles.auth_container}>
-      <div className={styles2.auth_img_container}>
-            <Image src="/logo/logo.svg" alt="logo_picture" fill style={{objectFit:'cover'}}/>
-    </div>
-      {/* if at login show this */}
+    }
+   }
 
-    <form onSubmit={(e)=>handleSubmition(e)} className={styles2.auth_form}>
-        {isLogin?<>
-        <div className={!isForget?styles2.input_container:styles2.input_container_forgot}>
-        <FormInput isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="email" name="email" placeHolder="Enter address" required={true}></FormInput>
-          <div className={!isForget ? styles2.pass_container:styles2.pass_container_forgot}>
-            {!isForget ?<>
-              <FormInput   isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="password" name="password" placeHolder="Password" required={true}></FormInput>
-              <button type="button" onClick={()=>{setIsForget(!isForget);resetFields()}} className={`${styles2.btn_anchor} ${styles2.forgot_pass}`} >Forgot password</button>
-             </>:
-             <p>Please enter the email address you'd like your<br/>
+   const handleAuthNavigation = ()=>{
+    if(isLogin && !isForget){
+      return <div className={styles2.have_an_account}>
+        <h1>Have you an account?</h1>
+        <button className={`${styles2.btn_anchor} ${styles2.back_to_sign_in}`} onClick={()=> {getRect();setIsLogin(false);resetFields();}}>Sign up</button>
+      </div>
+    }else{
+      return <button  className={`${styles2.btn_anchor} ${styles2.back_to_sign_in}`} onClick={()=> {getRect();setIsLogin(true);setIsForget(false);resetFields();}}>Back to sign in</button>
+    }
+   }
+
+   const handleForgetDisplay= ()=>{
+    if(isForget && isLogin){
+      return <p>Please enter the email address you'd like your<br/>
                 password reset information sent to
              </p>
-             }
-          </div>
-        </div>
-        <div className={styles2.sign_in_btn_container}>
-          <FormButton btnType="submit" btnContent={isForget?"Request reset link":"Sign up"}></FormButton>
-          {!isForget?
-          <div className={styles2.have_an_account}>
-            <h1>Have you an account?</h1>
-            <button className={`${styles2.btn_anchor} ${styles2.back_to_sign_in}`} onClick={()=> {getRect();setIsLogin(false);resetFields();}}>Sign up</button>
-          </div>:
-          <button  className={`${styles2.btn_anchor} ${styles2.back_to_sign_in}`} onClick={()=> {getRect();setIsLogin(true);setIsForget(false);resetFields();}}>Back to sign in</button>
-          }
-        </div>
-        </>:
-        <>
-    {/* if at signup show this */}
-      
-        
+    }else if(!isForget && isLogin){
+      return <> <FormInput isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="password" name="password" placeHolder="Password" required={true}></FormInput>
+              <button type="button" onClick={()=>{setIsForget(!isForget);resetFields()}} className={`${styles2.btn_anchor} ${styles2.forgot_pass}`} >Forgot password</button></>
+    }
+    return <FormInput errors={errors} setErrors={setErrors} test={true} isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="password" name="password" placeHolder="Password" required={true}></FormInput>
+   }
+
+
+
+
+  return(
+    <div key={isLogin? 1:2} className={styles.auth_container}>
+      <div ref={logo_ref} className={styles2.auth_img_container}>
+            <Image src="/logo/logo.svg" alt="logo_picture" fill style={{objectFit:'cover'}}/>
+    </div>
+
+    <form onSubmit={(e)=>handleSubmition(e)} className={styles2.auth_form}>
+        {!isLogin&&
+        <div className={styles2.fade_in} style={{
+          display:"inherit",
+          flexDirection:"inherit",
+          gap:"inherit",
+        }}>
       <FormInput setErrors={setErrors} errors={errors} test={true} isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="text" name="full_name" placeHolder="Full Name" ></FormInput>
         <DatePicker
          preventOpenOnFocus={false}
@@ -179,21 +227,26 @@ const prevRect = useRef<DOMRect | null>(null);
       
       <FormInput setErrors={setErrors} errors={errors} isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="text" name="location" placeHolder="Location" ></FormInput>   
       <RadioButton setValue={setForm} value={form} ></RadioButton>
-      <FormInput setErrors={setErrors} errors={errors} test={true} isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="email" name="email" placeHolder="Enter address" ></FormInput>
-      <FormInput setErrors={setErrors} errors={errors} test={true} isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="password" name="password" placeHolder="Enter password" ></FormInput>
-      {!isValid&&<div className={styles2.error_popup}>
+      </div>}
+      <div ref={inpt_ref}>
+      <div  className={!isForget?styles2.input_container:styles2.input_container_forgot}>
+        <FormInput errors={errors} setErrors={setErrors} test={true} isValid={isValid} setIsValid={setIsValid} setValue={setForm} value={form} type="email" name="email" placeHolder="Enter address" required={true}></FormInput>
+          <div  className={!isForget ? styles2.pass_container:styles2.pass_container_forgot}>
+            {handleForgetDisplay()}
+          </div>
+      </div>
+      </div>
+      {!isValid && !isLogin &&<div className={styles2.error_popup}>
         <ul>
           {errorList.map((err)=>{
             return <li key={errMap.get(err)}>- {err}</li>
           })}
         </ul>
         </div>}
-  
-      <div className={styles2.sign_up_btn_container}>
-        <FormButton btnType="submit" btnContent="Sign up"></FormButton>
-        <button  className={`${styles2.btn_anchor} ${styles2.back_to_sign_in}`} onClick={()=>{getRect();setIsLogin(true);resetFields();}}>Back to sign in</button>
-      </div>
-      </>}
+        <div className={styles2.sign_in_btn_container}>
+          <FormButton ref={button_ref} btnType="submit" btnContent={handleSubmitButton() || ""}></FormButton>
+          {handleAuthNavigation()}
+        </div>
     </form>
 
     </div>
