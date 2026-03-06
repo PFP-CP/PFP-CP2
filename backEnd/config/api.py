7857ac92c0ea93ac81c68411bbc9b93c057f5a6a
Calypso <haithemnamaane@gmail.com>
2026-03-06 17:56:36 +0100

    update config/api.py and added updated requirment.txt

--- backEnd/config/api.py
+++ backEnd/config/api.py
@@ -4,8 +4,9 @@ from ninja_jwt.routers.verify import verify_router
 
 API = NinjaAPI()
 
-API.add_router('/token', tags=['Auth'], router=obtain_pair_router)
+API.add_router("/token", tags=["Auth"], router=obtain_pair_router)
 API.add_router("/token", tags=["Auth"], router=verify_router)
-API.add_router("/Account/" , tags =['Authentication Page'] , router = "Accounts.api.router")
-API.add_router("/Houses/" , "Houses.api.router")
-API.add_router("/Posts/" , "Posts.api.router")
+API.add_router("/Account/", tags=["Authentication Page"], router="Accounts.api.router")
+API.add_router("/Houses/", "Houses.api.router")
+API.add_router("/Posts/", "Posts.api.router")
+API.add_router("/Reservations/", "Reservations.api.router")
