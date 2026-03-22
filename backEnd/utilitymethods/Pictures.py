2dea86f86ebbbb84b9767dd6cee49d2bbd047c1d
Clanker66 <aklim4r006@gmail.com>
2026-03-22 22:15:54 +0100

    fixed some problems mentioned in jira

--- backEnd/utilitymethods/Pictures.py
+++ backEnd/utilitymethods/Pictures.py
@@ -1,6 +1,17 @@
 import urllib.request
+"""instance is either an account or a house model/object"""
+"""field name is the name of the picture field"""
+"""upload_picture is the picture instance"""
+"""everything else should be self explanatory"""
+
+"""method to check if the file is a picture of not ,please add any picture extension you feel appropriate """
+def is_image(uploaded_file):
+    allowed_types = ["image/jpeg", "image/png", "image/webp", "image/svg+xml"]
+    return uploaded_file.content_type in allowed_types
 
 def upload_picture(instance, field_name: str, uploaded_picture):
+    if not is_image(uploaded_picture):
+        raise ValueError("file is not an image")
     field = getattr(instance, field_name)
     field.save(uploaded_picture.name, uploaded_picture, save=True)
     return getattr(instance, field_name).url
@@ -21,6 +32,8 @@ def get_picture_url(instance, field_name: str):
 
 
 def replace_picture(instance, field_name: str, uploaded_picture):
+    if not is_image(uploaded_picture):
+        raise ValueError("file is not an image")
     delete_picture(instance, field_name)
     return upload_picture(instance, field_name, uploaded_picture)
 
