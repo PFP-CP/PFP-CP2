034aa994c7e3e82178902a57a3d3f59b8ab639e1
Abdllahtt <abdellahtt2@gmail.com>
2026-04-01 13:42:39 +0100

    fix package problem

--- frontEnd/pfp_front/components/create_post_page_components/image_uploader.tsx
+++ frontEnd/pfp_front/components/create_post_page_components/image_uploader.tsx
@@ -1,17 +1,29 @@
 'use client';
-import { useEffect } from 'react';
-import { FileUploadWithPreview } from 'file-upload-with-preview';
-import 'file-upload-with-preview/dist/style.css';
+import { useEffect,useState,useRef, ChangeEvent } from 'react';
+import style from '@/styles/create_post_page_styles/img_uploader.module.css'
+
 
 export default function Uploader() {
-  useEffect(()=>{
-    const upload = new FileUploadWithPreview('nook_images');
-    console.log(upload);
-  },[])
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [images, setImages] = useState<object>({});
+  const imagesIDs = useRef(0);
+  const saveImages = (e:ChangeEvent)=>{
+    Object.keys(e.target.files).forEach((img)=>{
+      setImages((prev)=>{return {...prev,[imagesIDs.current]:e.target.files[img]}});
+      imagesIDs.current++;
+    })
+  }
+  console.log(images);
   return(
     <>
-      <div className="custom-file-container" data-upload-id="nook_images"></div>;
-      <button onClick={()=> {console.log(upload.cachedFileArray)}}>click me</button>
+      <div className={style.cu} onClick={()=> inputRef.current?.click()}>click</div>
+      <input style={{display:'none'}} onChange={(e:ChangeEvent)=>saveImages(e)} ref={inputRef} type="file" multiple={true} />
+      {
+      Object.keys(images).map((imgKey)=>{
+        return <img key={imgKey} src={URL.createObjectURL(images[imgKey])} id={imgKey} alt="image" />
+      })
+      }
     </>
   )
 }
+
